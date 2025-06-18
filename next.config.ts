4507a6d04e95919dabc96fd75b7299e4aa07956d
Enes Eren Demirpolat <enes_erendemirpolat@hotmail.com>
2025-06-18 10:30:20 +0300

    chore: next.config.js images.domains ve images.remotePatterns eklendi (uploads için)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["jenkins.eneseren.online"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "jenkins.eneseren.online",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
